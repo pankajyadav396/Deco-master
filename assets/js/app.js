030d964de813275de8f8c8b0ddc208994266aec2
Your Name <you@example.com>
2023-10-07 14:19:37 +0530

    add btn back to top

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,17 @@
- const preloader = document.getElementById("preloader");
-    
-    setTimeout(() => {
-    preloader.classList.add("d-none")
-    preloader.classList.add("overflow-hidden")
-    }, 1000)
+const preloader = document.getElementById("preloader");
 
-    
+setTimeout(() => {
+   preloader.classList.add("d-none")
+   preloader.classList.add("overflow-hidden")
+}, 1000)
+
+let backtotop = document.getElementById('backto-top')
+
+window.addEventListener("scroll", () => {
+   if (window.scrollY > 700) {
+      backtotop.style.display = "flex"
+   }
+   else {
+      backtotop.style.display = "none"
+   }
+})
